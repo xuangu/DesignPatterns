456fcdc88be47162011a1f176e13e83f0fc07730
guxuan <guxuan@hecom.cn>
2018-01-16 21:33:42 +0800

    工厂方法

--- SwiftDesignPatterns/SwiftDesignPatterns/FactoryMethod/Product.swift
+++ SwiftDesignPatterns/SwiftDesignPatterns/FactoryMethod/Product.swift
@@ -8,7 +8,60 @@
 
 import Foundation
 
-class Product {
-    var name
+class RentalCar {
+    private var nameBV: String
+    private var passengersBV: Int
+    private var priceBV: Float
     
+    fileprivate init(name: String, passengers: Int, price: Float) {
+        self.nameBV = name
+        self.passengersBV = passengers
+        self.priceBV = price
+    }
+    
+    final var name: String {
+        get {
+            return nameBV
+        }
+    }
+    
+    final var passengers: Int {
+        get {
+            return passengersBV
+        }
+    }
+    
+    final var price: Float {
+        get {
+            return priceBV
+        }
+    }
+    
+    class func createRentalCar(passengers: Int) -> RentalCar? {
+        var car: RentalCar?
+        switch passengers {
+        case 0...3:
+            car = Compact()
+        case 4...8:
+            car = SUV()
+        default:
+            car = nil
+        }
+        
+        return car
+    }
+}
+
+
+class Compact: RentalCar {
+    fileprivate init() {
+        super.init(name: "WV Golf", passengers: 3, price: 20)
+    }
+}
+
+
+class SUV: RentalCar {
+    fileprivate init() {
+        super.init(name: "SUV", passengers: 8, price: 75)
+    }
 }
